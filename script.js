9582c9e1b287b97e5955cd23d0ddce81c46af770
Yabets <119093891+y2cold@users.noreply.github.com>
2025-05-28 20:08:50 +0300

    added code

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navLinks = document.getElementById('nav-links');
+    menuToggle.addEventListener('click', function() {
+        navLinks.classList.toggle('active');
+    });
+});
