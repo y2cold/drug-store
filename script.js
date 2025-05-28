9f3ae3dea5dc20ff3b6af0f0b2a42f7cfa7812b9
Yabets <119093891+y2cold@users.noreply.github.com>
2025-05-28 20:31:29 +0300

    removed javascript

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.getElementById('menu-toggle');
-    const navLinks = document.getElementById('nav-links');
-    menuToggle.addEventListener('click', function() {
-        navLinks.classList.toggle('active');
-    });
-});
